b628ec97a9eadac93126e3e86f6c13b76055f912
Arthope <pochtawow345@gmail.com>
2022-08-09 10:58:01 +0300

    Init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    public event UnityAction<int, int> HealthChanged;
+    public int Health => _health;
+    private int _currentHealth;
+   
+
+    private void Start()
+    {
+        _currentHealth = _health;
+        HealthChanged?.Invoke(_health, _currentHealth);
+    }
+
+    public void Damage(int damage)
+    {
+        _health -= damage;
+        HealthChanged?.Invoke(_health, _currentHealth);
+    }
+
+    public void Heal(int health)
+    {
+        _health += health;
+        HealthChanged?.Invoke(_health, _currentHealth);
+    }
+
+}
